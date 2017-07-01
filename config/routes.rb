a2b2822f72f740589a190a3170e2dede6847da9d
Sairam <dolasairam123@outlook.com>
2017-07-01 10:29:00 +0530

    add edit , update ,show and index listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'articles/index'
+
+  get 'article/index'
+
   resources :articles
 # For details on the DSL available within this file, 
 #see http://guides.rubyonrails.org/routing.html
