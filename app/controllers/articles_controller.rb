adb7cde31bd0f0b97de7bf60b72f3c10f7ce5aa6
Sairam <dolasairam123@outlook.com>
2017-07-08 22:43:41 +0530

    listing of articles in pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ end
 
 	def create
 		@article =Article.new(article_params)
-		@article.user =User.first
+		@article.user = current_user
 		if @article.save
 
 			flash[:success] = "Article was successfully created"
