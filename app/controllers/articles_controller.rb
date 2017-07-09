bd9aee2b262e4278230a089f4f2317993f84ef46
Sairam <dolasairam123@outlook.com>
2017-07-09 07:35:00 +0530

    add sign in option

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -70,7 +70,7 @@ end
 		end	
 		
 	def require_same_user
-		if current_user !=@article.user
+		if current_user !=@article.user and !current_user.admin?
 		flash[:danger] = "you can only edit or delete your article"
 		redirect_to root_path
 		end
