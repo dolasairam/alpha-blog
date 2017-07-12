a8af76600d2825ff95ed8925ccf960449dbf86d2
Sairam <dolasairam123@outlook.com>
2017-07-12 05:45:13 +0530

    add categories model and tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 
 gravatar_id = Digest::MD5::hexdigest(user.email.to_s.downcase)
 size = options[:size]
-gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}? s=#{size}"
+gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 image_tag(gravatar_url, alt: user.username, class: "img-circle")
 
 end
