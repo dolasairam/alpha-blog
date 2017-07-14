db9d0cf57069f57b0b4456f96d13e7f96852bc9b
Sairam <dolasairam123@outlook.com>
2017-07-14 21:50:15 +0530

    Alpha Blog is created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,21 @@ class CategoriesController < ApplicationController
 		@category = Category.new
 	end
 	
+	def edit
+		@category = Category.find(params[:id])
+	end
+	
+	def update
+		@category = Category.find(params[:id])
+		if @category.update(category_params)
+			flash[:success] = "Category name was successfully updated"
+			redirect_to categories_path(@category)
+		end
+	end
+	
 	def show
+		@category = Category.find(params[:id])
+		@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
 	
 	end
 	
